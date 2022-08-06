fce0707cebf9979c58630bfd15ed24fc797f9537
JP <johnpaulmitra@yahoo.com>
2022-08-05 20:00:24 -0500

    Added Google OAuth, Updated Rts, Added New Layouts

--- routes/auth.js
+++ routes/auth.js
@@ -15,8 +15,10 @@ router.get('/google/callback', passport.authenticate('google', { failureRedirect
 // @desc    Log Out User
 // @route   /auth/logout
 router.get('/logout', (req, res) => {
-  req.logout()
-  res.redirect('/')
+  req.logout(function(err) {
+    if (err) { return next(err) }
+    res.redirect('/')
+  })
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,42 +1,56 @@
 const path = require('path')
 const express = require('express')
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
 const passport = require('passport')
 const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
 const connectDB = require('./config/db')
 
 // Load config
 dotenv.config({ path: './config/config.env' })
 
 // Passport config
-// require('./config/passport')(passport)
+require('./config/passport')(passport)
 
 connectDB()
 
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
+
+// Body-Parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
 // Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
+// Handlebars Helpers
+const { formatDate, stripTags, truncate } = require('./helpers/hbs')
+
 // Handlebars
-app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('.hbs', exphbs.engine({ helpers: {
+  formatDate,
+  stripTags,
+  truncate
+}, defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', '.hbs')
 
 // Sessions
-// app.use(session({
-//   secret: 'keyboard cat',
-//   resave: false,
-//   saveUninitialized: false
-// }))
+app.use(session({
+  secret: 'keyboard mouse',
+  resave: false,
+  saveUninitialized: false,
+  store: new MongoStore({ mongooseConnection: mongoose.connection })
+}))
 
 // Passport Middleware
-// app.use(passport.initialize())
-// app.use(passport.session())
+app.use(passport.initialize())
+app.use(passport.session())
 
 // Static Folder
 app.use(express.static(path.join(__dirname, 'public')))
@@ -44,6 +58,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Routes
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
 
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
